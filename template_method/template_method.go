405873640b0197bd6b76a4bf2f5229a47da3a53e
acmerfight <acmerfight@gmail.com>
2018-04-23 23:34:54 +0800

    add comment

--- template_method/template_method.go
+++ template_method/template_method.go
@@ -5,6 +5,8 @@ import "fmt"
 //这个不是真正的 template method ，因为调用 process 执行的是 Abstract 的 step2，step2 不能访问 Concrete 的数据。
 //如果要像 Python 一样实现真正的 template method 可以参考 http://hackthology.com/object-oriented-inheritance-in-go.html
 
+// Golang 的 sort 也是 template method 的一种好的实现 https://golang.org/src/sort/sort.go
+
 type Processor interface {
 	step1()
 	step2()

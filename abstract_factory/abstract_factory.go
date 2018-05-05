fbd3c5ff4b3fb9178f099cab6a316d483cf5f79d
acmerfight <acmerfight@gmail.com>
2018-05-05 17:21:51 +0800

    add abstract_factory.go

--- abstract_factory/abstract_factory.go
+++ abstract_factory/abstract_factory.go
@@ -2,40 +2,56 @@ package main
 
 import "fmt"
 
+// this is my concrete girl friend
 type GirlFriend struct {
 	nationality string
 	eyesColor   string
 	language    string
 }
 
-type AbstractFactory interface {
-	CreateMyLove() GirlFriend
+type Baby struct {
+	nationality string
 }
 
-type IndianGirlFriendFactory struct{}
+type AbstractFactory interface {
+	CreateMyLove() GirlFriend
+	CreateBaby() Baby
+}
 
-type KoreanGirlFriendFactory struct{}
+type IndianFactory struct{}
 
-func (a IndianGirlFriendFactory) CreateMyLove() GirlFriend {
+type KoreanFactory struct{}
+
+func (a IndianFactory) CreateMyLove() GirlFriend {
 	return GirlFriend{"Indian", "Black", "Hindi"}
 }
 
-func (a KoreanGirlFriendFactory) CreateMyLove() GirlFriend {
+func (a IndianFactory) CreateBaby() Baby {
+	return Baby{"Hindi"}
+}
+
+func (a KoreanFactory) CreateMyLove() GirlFriend {
 	return GirlFriend{"Korean", "Brown", "Korean"}
 }
 
-func getGirlFriend(typeGf string) GirlFriend {
+func (a KoreanFactory) CreateBaby() Baby {
+	return Baby{"Korean"}
+}
+
+func getFacotry(typeGf string) interface{} {
 	factoryMap := map[string]AbstractFactory{
-		"Indian": IndianGirlFriendFactory{},
-		"Korean": KoreanGirlFriendFactory{},
+		"Indian": IndianFactory{},
+		"Korean": KoreanFactory{},
 	}
-	return factoryMap[typeGf].CreateMyLove()
+	return factoryMap[typeGf]
 }
 
 func main() {
-	a := getGirlFriend("Indian")
-	fmt.Println(a.eyesColor)
-	b := getGirlFriend("Korean")
-	fmt.Println(b.eyesColor)
-
+	var factory interface{}
+	factory = getFacotry("Indian")
+	fmt.Println(factory.(IndianFactory).CreateMyLove().eyesColor)
+	fmt.Println(factory.(IndianFactory).CreateBaby().nationality)
+	factory = getFacotry("Korean")
+	fmt.Println(factory.(KoreanFactory).CreateMyLove().eyesColor)
+	fmt.Println(factory.(KoreanFactory).CreateBaby().nationality)
 }
